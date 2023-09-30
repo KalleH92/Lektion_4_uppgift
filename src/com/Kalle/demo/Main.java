43c486ee1bfa4c8f329be048a8bc0b9d70fd254b
KalleH92 <kalle.hagber92@hotmail.com>
2023-09-30 14:48:15 +0200

    Second commit

--- src/com/Kalle/demo/Main.java
+++ src/com/Kalle/demo/Main.java
@@ -1,4 +1,14 @@
 package com.Kalle.demo;
 
 public class Main {
+    public static void main(String[] args) {
+int age = 17;
+if (age < 18) {
+    System.out.println("Underage");
+}else{
+    System.out.println("Adult");
+}
+    }
+
+
 }
